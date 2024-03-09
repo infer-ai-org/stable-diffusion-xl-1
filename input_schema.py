bd062364fb340bc32ed79e6ead5068350555eac2
Anand <66644641+maraoai@users.noreply.github.com>
2024-03-09 00:06:41 -0800

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -4,5 +4,11 @@ INPUT_SCHEMA = {
         'required': True,
         'shape': [1],
         'example': ["Penguins having dinner"]
+    },
+    "num_inference_steps": {
+        'datatype': 'INT8',
+        'required': True,
+        'shape': [1],
+        'example': [40]
     }
 }
